0d7064bdf5297f9f203cc9e1bd6559a754f16195
Alec Sharp <alecsharpie@gmail.com>
2021-09-06 19:36:54 +1000

    add images generator to the app

--- app.py
+++ app.py
@@ -1,9 +1,44 @@
 import streamlit as st
+import os
+
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+import numpy as np
+
+images = os.listdir('images')
 
 
-fig, ax = plt.subplots()
+#fig, ax = plt.subplots()
 
 
-ax.imshow(face, cmap='gray')
+#ax.imshow(face, cmap='gray')
 
-st.pyplot(fig)
+size_list = []
+for idx, img in enumerate(images):
+    if img != '.DS_Store':
+        prison_img = mpimg.imread('images/' + img)
+        size_list.append(prison_img.shape)
+
+mean_height = np.mean([size[0] for size in size_list])
+
+height_changes = [(size[0] - mean_height) / size[0] for size in size_list]
+
+new_widths = [
+    (size[0][1] + (size[0][1] * size[1])) / 5
+    for size in zip(size_list, height_changes)
+]
+
+for idx, img in enumerate(images[:10]):
+    if img != '.DS_Store':
+        prison_img = mpimg.imread('images/' + img)
+        st.image('images/' + img, width = 300)
+
+
+#for idx, img in enumerate(images[:3]):
+#    prison_img = mpimg.imread('images/' + img)
+#    fig, ax = plt.subplots(figsize=(4, ))
+#    ax.axis('off')
+#   ax.imshow(prison_img, interpolation="nearest")
+#    fig.tight_layout()
+#    st.pyplot(fig)
