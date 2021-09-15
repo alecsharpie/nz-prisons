1fb3488f73bdd325377d5ce8645a2890062e7d07
Alec Sharp <alecsharpie@gmail.com>
2021-09-16 03:31:12 +1000

    styple app to multiple pages with custom colors

--- app.py
+++ app.py
@@ -1,42 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
+from streamlit_pages.streamlit_pages import MultiPage
 import os
-#from streamlit_pages.streamlit_pages import MultiPage
-
-st.set_page_config(
-    page_title="NZ's Prisons",
-    page_icon="🔑",
-    layout="wide",  # wide
-    initial_sidebar_state="auto")
-
-
-
-# def home():
-#     st.write("Welcome to home page")
-#     if st.button("Click Home"):
-#         st.write("Welcome to home page")
-
-
-# def about():
-#     st.write("Welcome to about page")
-#     if st.button("Click about"):
-#         st.write("Welcome to About page")
-
-
-# def contact():
-#     st.write("Welcome to contact page")
-#     if st.button("Click Contact"):
-#         st.write("Welcome to contact page")
-
-
-# # call app class object
-# app = MultiPage()
-# # Add pages
-# app.add_page("Home",home)
-# app.add_page("About",about)
-# app.add_page("Contact",contact)
-# app.run()
-
 
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
@@ -44,61 +9,88 @@ import matplotlib.image as mpimg
 import numpy as np
 import pandas as pd
 
+st.set_page_config(
+    page_title="NZ's Prisons",
+    page_icon="🔑",
+    layout="wide")
 
-prisons_df = pd.read_csv('data/NZ_Prisons_Master.csv')
 
-images = os.listdir('images')
+def home():
+    st.write("Welcome to home page")
 
-images = [img for img in images if img != '.DS_Store']
+def prisons():
 
-st.header("NZ's Prisons - Our Prisons")
+    prisons_df = pd.read_csv('data/NZ_Prisons_Master.csv')
 
-controls = st.columns(4)
+    to_convert_for_sort = ['escapes', 'deaths','contraband']
 
-region = controls[0].radio('Select a Region',
-                              ('All', 'Northern','Central','Lower North','Southern'))
-if region != 'All':
-    prisons_df = prisons_df[prisons_df['region'] == region]
+    for col in to_convert_for_sort:
+        prisons_df[col] = prisons_df[col].str.replace('[^0-9]','')
+        prisons_df[col] = pd.to_numeric(prisons_df[col], errors = 'coerce')
 
-security = controls[1].radio(
-    'Select a Security Level',
-    ('All', 'Low-Medium', 'High', 'Maximum'))
-if security != 'All':
-    prisons_df = prisons_df[prisons_df['max_security'] == security]
+    images = os.listdir('images')
 
-order = controls[3].radio('Order by',
-                             ('Total_Prisoners', 'Opened', 'Staff', 'Escape', 'Contraband'))
+    images = [img for img in images if img != '.DS_Store']
 
-prisons_df = prisons_df.sort_values(order.lower())
+    st.markdown("""---""")
+    st.markdown("""View options:""")
 
-components.html(
-    """<hr style="height:10px;border:none;color:#C70039;background-color:#C70039;" /> """
-)
 
-for idx, row in prisons_df.iterrows():
-    st.markdown(f"""
-                ## {row['prison']}""")
+    controls = st.columns(4)
 
-    st.markdown(f"""
-                [Website]({row['link']})  -
-                {row['address']}  -
-                {row['phone']}""")
+    region = controls[0].radio('Select a Region',
+                                ('All', 'Northern','Central','Lower North','Southern'))
+    if region != 'All':
+        prisons_df = prisons_df[prisons_df['region'] == region]
 
-    columns = st.columns(2)
+    security = controls[1].radio(
+        'Select a Security Level',
+        ('All', 'Low-Medium', 'High', 'Maximum'))
+    if security != 'All':
+        prisons_df = prisons_df[prisons_df['max_security'] == security]
 
-    columns[0].image('images/' + row['image'], width=300)
-    columns[0].write(f"Established: {row['opened']}")
-    columns[0].write(f"{row['beds']} Bed's and {row['staff']} Staff")
 
-    columns[1].markdown(f"""
-                        {row['total_prisoners']} {row['gender']} Prisoners\n
-                        Security: {row['max_security']}\n\n
+    order = controls[3].radio(
+        'Order by',
+        ('Total_Prisoners', 'Opened', 'Staff', 'Escapes', 'Contraband'))
 
-                        Avg Incidents per year:\n
-                        {row['escapes']} Escapes\n
-                        {row['deaths']} Deaths\n
-                        {row['contraband']} Contraband Items
-                        """)
-    components.html(
-        """<hr style="height:2px;border:none;color:#333;background-color:#333;" /> """
-    )
+    prisons_df = prisons_df.sort_values(order.lower(), ascending = False)
+
+    st.markdown("""---""")
+
+
+    for idx, row in prisons_df.iterrows():
+        st.markdown(f"""
+                    ## {row['prison']}""")
+
+        st.markdown(f"""
+                    [Website]({row['link']})  -
+                    {row['address']}  -
+                    {row['phone']}""")
+        st.write(f"Established: {row['opened']}")
+
+        columns = st.columns(2)
+
+        columns[0].image('images/' + row['image'], width=300)
+        columns[1].write(
+            f"{row['total_prisoners']} {row['gender']} Prisoners\n and {row['staff']} Staff"
+        )
+
+        columns[1].markdown(f"""
+                            \n
+                            Security: {row['max_security']}\n\n
+
+                            Avg Incidents per year:\n
+                            {row['escapes']} Escapes\n
+                            {row['deaths']} Deaths\n
+                            {row['contraband']} Contraband Items
+                            """)
+        st.markdown("""---""")
+
+
+# call app class object
+app = MultiPage()
+# Add pages
+app.add_page("Overview",home)
+app.add_page("Our Prisons",prisons)
+app.run()
